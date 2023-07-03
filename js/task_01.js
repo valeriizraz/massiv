66d4f3f42ec52bc88f096ca80b33dcaa9a5a52b3
valeriizraz <valeriizraz@gmail.com>
2023-07-03 14:42:26 +0200

    Исправил первое задание

--- js/task_01.js
+++ js/task_01.js
@@ -1,24 +1,21 @@
 'use strict' 
 
+// массив, который содержит все значения
 const allStudents = ['Иванов', 'Петров', 'Сидоров', 'Кузнецов', 'Смирнов', 'Попов', 'Соколов'];
+
+// массив значений, которые необходимо удалить
 const failedStudents = ['Сидоров', 'Смирнов', 'Попов'];
 
-const sortArray = (todos, failed) => {
+// сделать набор для хранения значений из failedStudents
+const namesToDeleteSet = new Set(failedStudents);
 
-  const todosSort = todos.sort();
-  const failedSort = failed.sort();
+// использовать метод filter()
+// фильтровать только те элементы
+// которые не нужно удалять из массива
+const newArr = allStudents.filter((name) => {
+  // вернуть те элементы, которых нет в nameToDeleteSet
+  return !namesToDeleteSet.has(name);
+});
 
-  for (let i = 0; i < todosSort.length; i ++) {
-    console.log(i);
-    for (let y = 0; y < failedSort.length; y++) {
-      console.log(y);
-    }
-  }
-
-  if (todosSort[i] !== failedSort[y]) {
-    const newTodos = todosSort.pop();  
-  }
-}
-
-sortArray(allStudents, failedStudents);
+console.log(newArr); // ["Lily", "Jessica"]
 
--- js/task_03.js
+++ js/task_03.js
@@ -1,12 +1,8 @@
 'use strict'
 
-const names= ['Noah', 'Liam', 'Mason', 'Jacob', 'Robot', 'William', 'Ethan', 'Michael', 'Alexander'];
-
-const addPrefix = (name) => {
-  let str = 'Mr ';
-  for (let i = 0; i < name.length; i++) {
-    str += str + name[i];
-  }
-}
-
-console.log(addPrefix(names));
+// const addPrefix = (name) => {
+//   let str = 'Mr ';
+//   for (let i = 0, pre = ; i < name.length; i++) {
+//     str += str + name[i];
+//   }
+// }
